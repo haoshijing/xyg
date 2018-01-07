2527656f8106a56eb40403395b213853d11c8108
haoshijing <haoshijing690@163.com>
2018-01-07 08:58:35 +0800

    咸鱼狗定制版

--- doc/data.sql
+++ doc/data.sql
@@ -1,3 +1,9 @@
 Insert into t_admin(id,userName ,password ,saltPassword,insertTime,lastUpdateTime ,status)
 select
  null , 'superadmin','74cb1c732d55c3aa93bad590ffe1ee8b','123',  unix_timestamp()*1000, unix_timestamp()*1000,1;
+INSERT INTO `pay_link` VALUES ('1', 'InTfIygzhw', '4112', '29800');
+INSERT INTO `pay_link` VALUES ('2', '0BA7HmcarO', '1510', '12800');
+INSERT INTO `pay_link` VALUES ('3', 'H3ZzXORrdF', '540', '5000');
+INSERT INTO `pay_link` VALUES ('4', 'vC1taJ2jRi', '300', '3000');
+INSERT INTO `pay_link` VALUES ('5', 'NE42anAhzj', '80', '800');
+INSERT INTO `pay_link` VALUES ('6', 'F066yjemS8', '20', '200');
